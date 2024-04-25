9fdee0728dbb84dc12a598abf2174eb41733fe62
DELL <kokatekruti95@gmail.com>
2024-04-25 14:00:50 +0530

    first

---
+++ variables.java
@@ -0,0 +1,20 @@
+public class variables {
+    public static void main(String[] args) {
+        // x is a local variable
+
+        int a = 10;
+
+        String message = "Hello";
+
+        System.out.println("a = " + a);
+        System.out.println("message = " + message);
+
+        if(a>=5)
+        {
+
+            String result = "a is greater than 5";
+            System.out.println(result);
+        }
+
+    }
+}
