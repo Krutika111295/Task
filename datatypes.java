893a323928c9c05de757fb5b52ff6354d763bd12
DELL <kokatekruti95@gmail.com>
2024-04-25 17:52:14 +0530

    first

---
+++ datatypes.java
@@ -0,0 +1,24 @@
+public class datatypes {
+    public static void main(String[] args)
+    {
+        int a=100;// int datatype
+        //char datatype
+        char c='a';
+        //string datatype
+        String ch="hello";
+        //boolean datatype
+        boolean b=true;
+        //float datatype
+        float f= 5.99F;
+        System.out.println(a);
+
+        System.out.println(c);
+
+        System.out.println(ch);
+
+        System.out.println(b);
+
+        System.out.print(f);
+
+    }
+}
